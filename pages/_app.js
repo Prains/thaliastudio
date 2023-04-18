dca358543f71748d8ffe36caf90caddeb0173cf3
prains <sleskov2013@yandex.ru>
2023-04-18 13:04:23 +0300

    add main to app layout

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,8 @@ import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <main className="flex items-center justify-center flex-col">
       <Component {...pageProps} />
-    </>
+    </main>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,3 @@
-import Anchor from "@/components/Anchor/Anchor";
-import Button from "@/components/Button/Button";
-import Paragraph from "@/components/Paragraph/Paragraph";
-import Title from "@/components/ui/Title/Title";
-
 export default function Home() {
-  return (
-<section className="flex items-center justify-center my-0 mx-auto w-full"><Title>123</Title></section>
-  )
+  return;
 }

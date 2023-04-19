ce33f59fde26e46612ddf082e1238797e27855cd
Prains <sleskov2013@yandex.ru>
2023-04-19 16:44:01 +0300

    code review

--- components/Homepage/Natural/Natural.jsx
+++ components/Homepage/Natural/Natural.jsx
@@ -1,25 +1,23 @@
-const { Title } = require("@/components/ui/ui");
+import { Title, Anchor } from "@/components/ui/ui";
 import Image from "next/image";
 import { jute, rafia } from "@/images/Mainpage/Soul/soul";
-import { LinkMore } from "./LinkMore";
 
 const Natural = () => {
   return (
-    <div className="w-[87%] text-center mb-20">
+    <section className="w-[87%] text-center mb-20">
       <Title className="text-[32px] mb-10">Натуральные материалы</Title>
       <div className="grid grid-row-3 justify-items-center">
         <div className="w-[220px] mb-[30px]">
           <Image className="mb-[15px]" src={jute} alt="Джут" />
           <h4 className="text-[24px] mb-2.5">Джут</h4>
-          <LinkMore />
+          <Anchor href={"/"}>123</Anchor>
         </div>
         <div className="w-[220px]">
           <Image className="mb-[15px]" src={rafia} alt="Рафия" />
           <h4 className="text-[24px] mb-2.5">Рафия</h4>
-          <LinkMore />
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 export { Natural };
--- components/Homepage/Soul/Soul.jsx
+++ components/Homepage/Soul/Soul.jsx
@@ -1,15 +1,16 @@
-const { Title } = require("@/components/ui/ui");
+const { Title } = require("@/components/ui/ui"); // импортировать как модуль ES6
 import Image from "next/image";
-import { soulJute, soulRafia, soulHandmade } from "@/images/Icons/Icons";
+import { soulJute, soulRafia, soulHandmade } from "@/images/Icons/Icons"; // неправильная структура иконок
 
 const Soul = () => {
   return (
-    <div className="w-[87%] text-center mb-20">
+    <section className="w-[87%] text-center mb-20">
       <Title className="text-[32px] mb-10">Для души и красоты</Title>
       <p className="text-[14px] mb-[30px]">
         Thalia Studio - это изделия из натуральных материалов таких, как джут и
         рафия. Это красивые сумочки, созданные с душой и любовью к своему делу.
       </p>
+      {/*использовать параграф из ui kit*/}
       <div className="grid grid-row-3 justify-items-center">
         <div className="w-[220px]">
           <Image className="mb-[25px]" src={soulJute} alt="Джут" />
@@ -20,17 +21,19 @@ const Soul = () => {
         </div>
         <div className="w-[220px]">
           <Image className="mb-[25px]" src={soulHandmade} alt="Ручная работа" />
+          {/* неподробный и не детальный атрибут alt*/}
           <h4 className="text-[24px] mb-[30px]">Ручная работа</h4>
         </div>
         <div className="w-[220px]">
           <Image className="mb-[25px]" src={soulRafia} alt="Рафия" />
+          {/* неподробный и не детальный атрибут alt*/}
           <h4 className="text-[24px] mb-2.5">Рафия</h4>
           <p className="font-text text-[14px]">
             Натуральное волокно из листьев пальмы
           </p>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 export { Soul };

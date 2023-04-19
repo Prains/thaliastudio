09d80969d59bd5fe2897d8428b76a9303f48c0d1
prains <sleskov2013@yandex.ru>
2023-04-19 12:45:26 +0300

    fix anchor from ui

--- components/ui/Anchor/Anchor.jsx
+++ components/ui/Anchor/Anchor.jsx
@@ -1,19 +1,20 @@
 import Link from "next/link";
 
-const NotALink = ({ children, className }) => {
+const NotALink = ({ children, className, onClick }) => {
   return (
     <p
       className={
         `flex items-center justify-center gap-[16px] text-textDefault border-b-2 border-black border-dashed max-w-max text-sm font-text hover:border-solid hover:font-semibold active:text-textActive active:border-dashed` +
         ` ${className}`
       }
+      onClick={onClick}
     >
       {children}
     </p>
   );
 };
 
-const Anchor = ({ children, href, className, link }) => {
+const Anchor = ({ children, href, className, link, onClick }) => {
   if (link) {
     return (
       <Link
@@ -28,7 +29,11 @@ const Anchor = ({ children, href, className, link }) => {
     );
   }
 
-  return <NotALink className={className}>{children}</NotALink>;
+  return (
+    <NotALink className={className} onClick={onClick}>
+      {children}
+    </NotALink>
+  );
 };
 
 export default Anchor;

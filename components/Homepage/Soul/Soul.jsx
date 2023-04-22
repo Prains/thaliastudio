924da5294b17f660a84a3ac600c9ed0f6ebbf036
PchelaMaya <pchelaamaya@gmail.com>
2023-04-22 19:59:27 +0300

    fix bag soul

--- components/Homepage/Soul/Soul.jsx
+++ components/Homepage/Soul/Soul.jsx
@@ -3,26 +3,26 @@ import Image from "next/image";
 import { soulJute, soulRafia, soulHandmade, soulRopeRight } from "@/images/Mainpage/Soul/soul";
 const Soul = () => {
   return (
-    <section className="w-[87%] text-center mb-20 md:w-[69%] ">
-      <Title className="text-[32px] mb-10 md:mx-auto md:mb-[70px] md:text-[70px]">
+    <section className="w-[87%] text-center mb-20 lg:w-[69%] lg:mb-[150px]">
+      <Title className="text-[32px] mb-10 lg:mx-auto lg:mb-[70px] lg:text-[70px]">
         Для души и красоты
       </Title>
       <Paragraph 
         defaultSize 
-        className="mb-[30px] md:mx-auto md:mb-[70px] md:w-[625px] md:text-[16px] md:flex md:justify-center"
+        className="mb-[30px] lg:mx-auto lg:mb-[60px] lg:w-[625px] lg:text-[16px] lg:flex lg:justify-center"
       >
         Thalia Studio - это изделия из натуральных материалов таких, как джут и
         рафия. Это красивые сумочки, созданные с душой и любовью к своему делу.
       </Paragraph>
       <article className="grid grid-row-3 justify-items-center">
-        <div className="w-[220px] md:flex md:items-center md:gap-x-[100px] md:w-[590px] md:relative md:right-[-180px]">
+        <div className="w-[220px] lg:flex lg:items-center lg:gap-x-[100px] lg:w-[590px] lg:relative lg:right-[-180px]">
           <Image 
             className="mb-[25px]" 
             src={soulJute} 
             alt="Бежево-серые нитки Джута" 
           />
-          <div className="md:w-[190px] md:text-left md:relative md:top-[-25px]">
-            <h4 className="font-title font-semibold text-[24px] mb-2.5 md:text-[30px]">
+          <div className="lg:w-[190px] lg:text-left lg:relative lg:top-[-40px]">
+            <h4 className="font-title font-semibold text-[24px] mb-2.5 lg:text-[30px]">
               Джут
             </h4>
             <Paragraph 
@@ -33,24 +33,24 @@ const Soul = () => {
             </Paragraph>
           </div>
         </div>
-        <div className="md:flex md:relative md:top-[-180px] md:right-[180px]">
+        <div className="lg:flex lg:relative lg:top-[-195px] lg:right-[200px]">
           <Image 
-            className="mb-[25px] w-[220px] md:relative md:right-[-80px] md:top-[115px] md:text-left" 
+            className="mb-[25px] w-[220px] mx-auto lg:w-[300px] lg:relative lg:right-[-80px] lg:top-[115px] lg:text-left" 
             src={soulHandmade} 
             alt="Печенье и сумка ручной работы" 
           />
-          <h4 className="font-title leading-[130%] font-semibold text-[24px] mb-[30px] md:text-[30px] md:relative md:right-[190px] md:top-[35px] md:w-[178px] md:text-left">
+          <h4 className="font-title leading-[130%] font-semibold text-[24px] mb-[30px] lg:text-[30px] lg:relative lg:right-[270px] lg:top-[0px] lg:w-[178px] lg:text-left">
             Ручная работа
           </h4>
         </div>
-        <div className="w-[220px] md:w-[409px] md:flex md:items-center md:relative md:top-[-395px] md:right-[-275px]">
+        <div className="lg:flex lg:items-center lg:relative lg:top-[-520px] lg:right-[-385px]">
           <Image 
-            className="mb-[25px] w-[220px]" 
+            className="mb-[25px] w-[220px] mx-auto lg:w-[300px]" 
             src={soulRafia} 
             alt="Бежевые волокна рафии" 
           />
-          <div className="md:w-[190px] md:relative md:top-[165px] md:right-[310px] md:text-left">
-            <h4 className="font-title font-semibold text-[24px] mb-2.5 md:text-[30px]">
+          <div className="lg:w-[190px] lg:relative lg:top-[220px] lg:right-[430px] lg:text-left">
+            <h4 className="font-title font-semibold text-[24px] mb-2.5 lg:text-[30px]">
               Рафия
             </h4>
             <Paragraph 

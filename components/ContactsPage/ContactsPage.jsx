670651b5c671be8452911441e336ece619c5f018
Igor Ershov <privetie@yandex.ru>
2023-04-25 18:50:52 +0300

    contact page

--- components/ContactsPage/ContactsPage.jsx
+++ components/ContactsPage/ContactsPage.jsx
@@ -1,21 +1,56 @@
 import React from "react";
 import { Title, Paragraph } from "@/components/ui/ui";
+import ContactLink from "@/components/ContactsPage/ContactLink/ContactLink";
+import { ContactPhoneBlock } from "./ContactPhoneBlock/ContactPhoneBlock";
+
+import {
+  telegram as telegramImg,
+  vk as vkImg,
+  instagram as instagramImg,
+  whatsapp as whatsappImg,
+} from "@/images/icons/icons";
 
 export const ContactsPage = () => {
+  const socials = [
+    {
+      social: "telegram",
+      img: telegramImg,
+      link: "https://t.me/thalia_crochetstudio",
+      title: "@thalia_crochetstudio",
+    },
+    {
+      social: "vk",
+      img: vkImg,
+      link: "/",
+      title: "Thalia Studio",
+    },
+    {
+      social: "instagram",
+      img: instagramImg,
+      link: "/",
+      title: "@thalia_crochetstudio",
+    },
+    {
+      social: "whatsapp",
+      img: whatsappImg,
+      link: "/",
+      title: "Natalia Leskova",
+    },
+  ];
   return (
-    <div className="flex flex-col items-center w-[87%] xl:w-[69%]">
-      <Title className="text-[32px] mb-[40px] mx-auto lg:text-[70px] lg:mb-[70px]">
+    <div className="flex flex-col items-center mt-[30px] w-[87%] xl:w-[69%]">
+      <Title className="text-[32px] mb-[40px] mx-auto leading-[110%] lg:text-[70px] lg:mb-[70px]">
         Контакты
       </Title>
       <Paragraph className="text-center text-[14px] leading-[130%] mb-[35px] lg:mb-[60px]">
         Для связи с нами напишите или позвоните любым удобным для Вас способом
       </Paragraph>
-      <div className="grid grid-rows-2 grid-cols-2">
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
-        <div>4</div>
-      </div>
+      <ul className="grid grid-rows-2 grid-cols-2 gap-x-[20px] gap-y-[30px] mb-[40px] md:grid-rows-1 md:grid-cols-4 xl:mb-[70px]">
+        {socials.map((social, i) => (
+          <ContactLink key={i} {...social} />
+        ))}
+      </ul>
+      <ContactPhoneBlock />
     </div>
   );
 };

689d7ccdd8656641896e8709050c7fea7970498f
Prains <sleskov2013@yandex.ru>
2023-04-25 22:07:14 +0300

    format payment

--- pages/payment.js
+++ pages/payment.js
@@ -2,7 +2,13 @@ import PaymentPage from "@/components/PaymentPage/Payment";
 import { paymentTop, paymentBottom } from "@/utils/utils";
 
 const Payment = () => {
-  return <PaymentPage top={paymentTop} bottom={paymentBottom} title='Оплата и доставка'/>;
+  return (
+    <PaymentPage
+      top={paymentTop}
+      bottom={paymentBottom}
+      title="Оплата и доставка"
+    />
+  );
 };
 
 export default Payment;

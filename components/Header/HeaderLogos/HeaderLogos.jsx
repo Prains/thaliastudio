12592c247c28f2f195ad487749fc0ec5f559db50
Prains <sleskov2013@yandex.ru>
2023-04-26 15:46:30 +0300

    fix header logo

--- components/Header/HeaderLogos/HeaderLogos.jsx
+++ components/Header/HeaderLogos/HeaderLogos.jsx
@@ -6,10 +6,10 @@ import { url } from "@/utils/url";
 const HeaderLogos = () => {
   return (
     <>
-      <Link href={url.index}>
+      <Link href={url.index} className="md:hidden">
         <Image src={logoMobile} alt="Лого thaliastudio" className="md:hidden" />
       </Link>
-      <Link href={url.index}>
+      <Link href={url.index} className="hidden md:block">
         <Image
           src={logoDesktop}
           alt="Лого thaliastudio"

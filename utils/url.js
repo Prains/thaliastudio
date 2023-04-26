807083923d5f161b39d347bf2f6f05fda0d23b5b
Prains <sleskov2013@yandex.ru>
2023-04-26 15:15:37 +0300

    edit url js

--- utils/url.js
+++ utils/url.js
@@ -5,3 +5,14 @@ const url = {
   paymnet: "/payment",
   return: "/return",
 };
+
+const tel = "+7 977 749 52 70";
+
+const socialLinks = {
+  vk: "https://vk.com/thaliastudio",
+  tg: "https://t.me/thalia_crochetstudio",
+  ig: "https://www.instagram.com/thalia_crochetstudio/",
+  wa: "wa.me/+79161421453",
+};
+
+export { url };

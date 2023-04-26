801b59b79f7231406f82ff0141f7cb8f8416ea86
Prains <sleskov2013@yandex.ru>
2023-04-26 21:51:14 +0300

    add preloader

--- components/ui/Preloader/Preloader.jsx
+++ components/ui/Preloader/Preloader.jsx
@@ -3,11 +3,13 @@ import { preloaderImage } from "@/images/Preloader/PreloaderImage";
 
 const Prealoder = () => {
   return (
-    <Image
-      src={preloaderImage}
-      alt="Огромный крутящийся клубок джута"
-      className="animate-spin"
-    />
+    <div className="w-full h-full flex items-center justify-center absolute top-0 left-0">
+      <Image
+        src={preloaderImage}
+        alt="Огромный крутящийся клубок джута"
+        className="animate-spin"
+      />
+    </div>
   );
 };
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,26 @@
 import Layout from "@/components/Layout/Layout";
+import Prealoder from "@/components/ui/Preloader/Preloader";
 import "@/styles/globals.css";
+import { useEffect, useState } from "react";
 
 export default function App({ Component, pageProps }) {
+  const [loading, isLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      isLoading(false);
+    }, 700);
+  }, []);
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      {loading ? (
+        <Prealoder />
+      ) : (
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      )}
+    </>
   );
 }

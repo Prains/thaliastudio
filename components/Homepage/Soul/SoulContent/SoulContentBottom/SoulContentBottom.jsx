986bdc5a68b5a2d2a491f14d7b8652d48b4a1f49
PchelaMaya <pchelaamaya@gmail.com>
2023-04-28 18:04:14 +0300

    fixed horizontal scroll in soul block

--- components/Homepage/Soul/SoulContent/SoulContentBottom/SoulContentBottom.jsx
+++ components/Homepage/Soul/SoulContent/SoulContentBottom/SoulContentBottom.jsx
@@ -4,13 +4,13 @@ import { soulRafia } from "@/images/Mainpage/Soul/soul";
 
 const SoulContentBottom = () => {
   return (
-    <article className="lg:flex lg:items-center lg:relative lg:top-[-520px] lg:right-[-385px]">
+    <article className="lg:flex lg:flex-col lg:items-center lg:relative lg:top-[-520px] lg:right-[-290px]">
       <Image
         className="mb-[25px] w-[220px] mx-auto lg:w-[300px]"
         src={soulRafia}
         alt="Бежевые волокна рафии"
       />
-      <div className="lg:w-[190px] lg:relative lg:top-[220px] lg:right-[430px] lg:text-left">
+      <div className="lg:w-[190px] lg:relative lg:top-[-50px] lg:right-[180px] lg:text-left">
         <h4 className="font-title font-semibold text-[24px] mb-2.5 lg:text-[30px]">
           Рафия
         </h4>
